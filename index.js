991f8daf6d67dce576407b94eb604ccceda7bf7b
amird55 <amir.dgn@outlook.com>
2023-03-10 10:25:19 +0200

    adding board

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use('/CSS', express.static(__dirname + "/CSS"));
 
+var board=[];
+createBoard();
+dropCoin(1,2);
+console.log(board);
 const router = express.Router();
 var cnt=0;
 router.get('/', (req, res) => {        //get requests to the root ("/") will route here
@@ -26,3 +30,19 @@ app.listen(port, () => {            //server starts listening for any attempts f
     console.log(`Now listening on port ${port}`); 
 });
 
+function createBoard(){
+  for(let r=0;r<6;r++){
+    board[r]=[];
+    for(let c=0;c<7;c++){
+      board[r][c]=0;
+    }
+  }
+}
+function dropCoin(col,plyr){
+  for(let r=board.length-1;r>=0;r--){
+    if(board[r][col]==0){
+      board[r][col]=plyr;
+      break;
+    }
+  }
+}
